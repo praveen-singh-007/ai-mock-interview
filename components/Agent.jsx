3905a5406d15585a30f2cfff98d6176b84eb558d
Praveen Singh <166694190+praveen-singh-007@users.noreply.github.com>
2026-05-06 23:54:59 +0530

    Simplify Vapi start call by removing overrides

--- components/Agent.jsx
+++ components/Agent.jsx
@@ -71,17 +71,7 @@ const Agent = ({ userName, userId, type }) => {
       CallStatus.CONNECTING
     );
 console.log("Starting Vapi with squadId", "38741758-f1c0-4473-b79c-92bc58173942");
-   await vapi.start({
-  assistant: {
-    assistantId: "2089cb85-f20f-438c-9189-f17d12020a31",
-    assistantOverrides: {
-      variableValues: {
-        username: userName,
-        userid: userId,
-      },
-    },
-  },
-});
+   await vapi.start("2089cb85-f20f-438c-9189-f17d12020a31");
 
   } catch (error) {
 
